1fb1b488da147d6c029ab57c8de1dc3f2522bb18
takeruhiura <157721288+takeruhiura@users.noreply.github.com>
2025-10-16 12:31:23 -0400

    Update multiseg_driver_tb.v

--- multiseg_driver_tb.v
+++ multiseg_driver_tb.v
@@ -19,28 +19,29 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module multiseg_driver_tb;
-
     reg clk;
     reg [15:0] bcd_in;
     wire [3:0] seg_anode;
     wire [6:0] seg_cathode;
-    
-    multiseg_driver uut(.clk(clk),.bcd_in(bcd_in),.seg_anode(seg_anode),.seg_cathode(seg_cathode));
-    
+
+    multiseg_driver uut (
+        .clk(clk),
+        .bcd_in(bcd_in),
+        .seg_anode(seg_anode),
+        .seg_cathode(seg_cathode)
+    );
+
+    always #1 clk = ~clk;
+
     initial begin
-        clk=0;
-        #10 clk=~clk;
-    end
-    
-    initial begin
-        bcd_in={4'd1,4'd2,4'd3,4'd4};
-        #10;
-        bcd_in={4'd9,4'd8,4'd7,4'd6};
-        #10;
-        
+        clk = 0;
+        bcd_in = {4'd1, 4'd2, 4'd3, 4'd4};
+        #10241;  
+       
+        bcd_in = {4'd9, 4'd8, 4'd7, 4'd6};
+        #10241;
         $stop;
-     end
-     
+    end
 endmodule
+     

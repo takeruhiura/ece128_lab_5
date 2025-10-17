983412d7628e6778a8f1fe20d80cdf9f8bab3c14
takeruhiura <157721288+takeruhiura@users.noreply.github.com>
2025-10-16 21:28:49 -0400

    Update multiseg_driver.v

--- multiseg_driver.v
+++ multiseg_driver.v
@@ -6,7 +6,7 @@
 // Create Date: 09/16/2025 01:41:18 PM
 // Design Name: multiseg driver
 // Module Name: bcd_seven_s
-// Project Name: ECE 128 Lab 4
+// Project Name: ECE 128 Lab 5
 // Target Devices: 
 // Tool Versions: 
 // Description: This is the module to implement the driver
@@ -29,5 +29,6 @@ output [6:0] seg_cathode
 wire [3:0] bcd_val;
 anode_generator AG(.clk(clk),.bcd_in(bcd_in),.seg_anode(seg_anode),.bcd_out(bcd_val));
 seven_seg_decoder SSD(.data_in(bcd_val),.seg(seg_cathode));
+
 
 endmodule;
